04a8096e8a0fc55b3ec3a3d365aeedc9fe64aa77
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-02-28 21:47:19 +0200

    refactor: routes were refactored

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from "express";
-import { getProductsRouter } from "./routes/productsRouter";
-import { getAddressesRouter } from "./routes/addressesRouter";
-import { getTestsRouter } from "./routes/testRouter";
+import productsRouter from "./routes/productsRouter";
+import addressesRouter from "./routes/addressesRouter";
+import testRouter from "./routes/testRouter";
 
 export const app = express();
 
 const jsonBodyMiddleware = express.json();
 app.use(jsonBodyMiddleware);
 
-app.use("/products", getProductsRouter());
-app.use("/addresses", getAddressesRouter());
-app.use("/__test__", getTestsRouter());
+app.use("/products", productsRouter);
+app.use("/addresses", addressesRouter);
+app.use("/__test__", testRouter);
--- src/routes/testRouter.ts
+++ src/routes/testRouter.ts
@@ -2,16 +2,14 @@ import express from "express";
 import { HTTP_STATUSES } from "../http_statuses";
 import { testsRepository } from "../repositories/testsRepository";
 
-export const getTestsRouter = () => {
-  const router = express.Router();
-  router.delete("/products", (_, res) => {
-    testsRepository.clearProductsArray();
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-  });
-  router.delete("/addresses", (_, res) => {
-    testsRepository.clearAddressesArray();
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-  });
+const router = express.Router({});
+router.delete("/products", (_, res) => {
+  testsRepository.clearProductsArray();
+  res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
+router.delete("/addresses", (_, res) => {
+  testsRepository.clearAddressesArray();
+  res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
 
-  return router;
-};
+export default router;
