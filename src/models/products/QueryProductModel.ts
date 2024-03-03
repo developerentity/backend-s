8cbfcbb4b907e606c30a4959debaa504f5ff5461
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-03-03 19:55:11 +0200

    feat: sorting ability was added to products endpoint

--- src/models/products/QueryProductModel.ts
+++ src/models/products/QueryProductModel.ts
@@ -1,6 +1,8 @@
 export type QueryProductModel = {
   pageSize: string;
   pageNumber: string;
+  sortField: string;
+  sortOrder: string;
   /**
    * This title should be included in Title of found Products
    */
