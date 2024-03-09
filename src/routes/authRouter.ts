ad191a8a81e3efd89e80b7db0573de32911ff01a
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-03-09 13:16:03 +0200

    WIP: implementation JWT in progress

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 import { usersService } from "../domain/usersService";
 import { HTTP_STATUSES } from "../http_statuses";
 import { loginValidator } from "../validators/loginValidator";
 import { inputValidationMiddleware } from "../validators/inputValidationMiddleware";
+
+export const secretKey =
+  "4715aed3c946f7b0a38e6b534a9583628d84e96d10fbc04700770d572af3dce43625dd";
 
 export const authRouter = Router({});
 
@@ -18,7 +22,8 @@ authRouter.post(
       password
     );
     if (checkResult) {
-      return res.status(HTTP_STATUSES.OK_200).json({ message: "Authorized" });
+      const token = jwt.sign({ loginOrEmail }, secretKey, { expiresIn: "1h" });
+      return res.status(HTTP_STATUSES.OK_200).json({ token });
     } else {
       return res
         .status(HTTP_STATUSES.UNAUTHORIZED_401)
