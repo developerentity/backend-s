5907ba657cc2c7ff9cd1ee1e9264f5b9812022c1
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-03-11 21:32:31 +0200

    feat: Atlas MongoDB was integrated

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,5 +1,6 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
+import { URI } from "../config";
 import { UserDBType } from "../domain/usersService";
 
 export type ProductType = {
@@ -9,23 +10,30 @@ export type ProductType = {
   createdAt: number;
 };
 
-const mongoDB = process.env.mongoURI || "mongodb://0.0.0.0:27017";
+const uri = URI || "";
 
-const client = new MongoClient(mongoDB);
-
-const db = client.db("shop");
-
-export const productsCollection = db.collection<ProductType>("products");
-export const usersCollection = db.collection<UserDBType>("users");
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 export async function runDB() {
   try {
     // Connect the client to the server
-    await client.db("products").command({ ping: 1 });
-    await client.db("users").command({ ping: 1 });
-    console.log("Connected successfully to mongo server");
-  } catch {
-    console.log("Can't connect to DB");
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinger your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (err) {
+    console.log("Can't connect to DB: " + err);
     await client.close();
   }
 }
+
+const db = client.db("shop");
+export const productsCollection = db.collection<ProductType>("products");
+export const usersCollection = db.collection<UserDBType>("users");
